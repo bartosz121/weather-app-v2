55b4ea35d0a03c740fd13accae83d41d18adb2e9
bartosz121 <bmagiera121@gmail.com>
2023-01-30 21:39:41 +0100

    Add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import Map from "./pages/Map";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <QueryClientProvider client={queryClient}>
+        <Map />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </div>
+  );
 }
 
 export default App;
