2405b60cf7325482569ba992ef4bbd118d4a3a30
bartosz121 <bmagiera121@gmail.com>
2023-02-11 23:29:01 +0100

    Add `SunDayInfoSection` component

---
+++ src/components/SunDayInfoSection.tsx
@@ -0,0 +1,42 @@
+import { WiSunrise, WiSunset } from "react-icons/wi";
+
+import { dtFormatHHMMWTimezone } from "../utils";
+import DisplayDt from "./DisplayDt";
+import ForecastSection from "./ForecastSection";
+
+type Props = React.HTMLAttributes<HTMLDivElement> & {
+  sunRise: number;
+  sunSet: number;
+  locationTimezone: string;
+};
+
+function SunDayInfoSection({
+  sunRise,
+  sunSet,
+  locationTimezone,
+  ...props
+}: Props) {
+  const dtFormat = dtFormatHHMMWTimezone(locationTimezone);
+  return (
+    <ForecastSection {...props}>
+      <div className="mt-1 md:px-2">
+        <div>
+          <h4 className="font-medium">Sunrise</h4>
+          <span className="flex flex-row items-center gap-2">
+            <WiSunrise className="h-16 w-16" />
+            <DisplayDt dt={sunRise} dtFormat={dtFormat} />
+          </span>
+        </div>
+        <div>
+          <h4 className="font-medium">Sunset</h4>
+          <span className="flex flex-row items-center gap-2">
+            <WiSunset className="h-16 w-16" />
+            <DisplayDt dt={sunSet} dtFormat={dtFormat} />
+          </span>
+        </div>
+      </div>
+    </ForecastSection>
+  );
+}
+
+export default SunDayInfoSection;
