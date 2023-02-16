a3bfc29e09408002ea7ae1bebe2aa62337f4a258
bartosz121 <bmagiera121@gmail.com>
2023-02-16 20:49:11 +0100

    Add extraClassName prop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,16 +3,23 @@ import clsx from "clsx";
 type Props = {
   children: React.ReactNode;
   flexSize?: string;
+  extraClassName?: string;
   onClick?: () => void;
 };
 
-function Card({ children, flexSize, onClick: onClickProp }: Props) {
+function Card({
+  children,
+  flexSize,
+  extraClassName,
+  onClick: onClickProp,
+}: Props) {
   return (
     <div
       onClick={onClickProp && onClickProp}
       className={clsx(
         flexSize ? flexSize : "shrink-0 grow basis-44",
-        "cursor-pointer border border-black bg-slate-700 bg-opacity-50 text-center hover:bg-opacity-100"
+        "border border-black bg-slate-700 bg-opacity-50 text-center hover:bg-opacity-100",
+        extraClassName ? extraClassName : null
       )}
     >
       {children}
