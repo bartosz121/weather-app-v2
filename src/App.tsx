982658206becaa9efeaf53d24128bfd9e9712d85
bartosz121 <bmagiera121@gmail.com>
2023-03-02 20:19:10 +0100

    Wrap router in AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AnimatePresence } from "framer-motion";
 
 import { router } from "./router";
 
@@ -10,7 +11,9 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AnimatePresence mode="wait">
+        <RouterProvider router={router} />
+      </AnimatePresence>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
